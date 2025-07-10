5348de82e2a2ad4470540461fd78ea2bd121b193
ramyasunarkani <ramyakrishnasunarkani@gmail.com>
2025-07-10 12:20:25 +0530

    routes

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
-const expres=require('express');
+const express = require("express");
+const app = express();
 
-const app=expres();
-
- function addUserProperty(req,res,next){
-    req.user="Guest";
-    next();
- }
- app.get('/welcome', addUserProperty, (req, res) => {
-    res.send(`<h1>Welcome, ${req.user}!</h1>`);
+app.get('/orders', (req, res) => {
+    res.send('<h1>Here is the list of all orders.</h1>');
 });
-app.listen(3000,()=>{
-    console.log(`Server is running on http://localhost:3000`);
+
+app.post('/orders', (req, res) => {
+    res.send('<h1>A new order has been created.</h1>');
+});
+
+app.get('/users', (req, res) => {
+    res.send('<h1>Here is the list of all users.</h1>');
+});
+
+app.post('/users', (req, res) => {
+    res.send('<h1>A new user has been added.</h1>');
+});
+
+
+app.listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
 });
