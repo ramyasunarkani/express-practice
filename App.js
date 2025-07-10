48e2d03944aa9e87f612ea49d825f02352bb52cd
ramyasunarkani <ramyakrishnasunarkani@gmail.com>
2025-07-10 11:51:54 +0530

    sec

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
 const expres=require('express');
 
 const app=expres();
-app.use((req,res,next)=>{
-    console.log("Server is up and running on port 3000! Ready to handle requests.");
 
-})
-app.listen(3000);
+ function addUserProperty(req,res,next){
+    req.user="Guest";
+    next();
+ }
+ app.get('/welcome', addUserProperty, (req, res) => {
+    res.send(`<h1>Welcome, ${req.user}!</h1>`);
+});
+app.listen(3000,()=>{
+    console.log(`Server is running on http://localhost:3000`);
+});
